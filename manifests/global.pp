1aa8e7ad5ad70358e4bba3963c0cacf297a57ef2
Matt Heath <matt@mattheath.com>
2014-05-12 13:30:49 +0100

    Change global to use ensure_resource so we don't need predefined classes

--- manifests/global.pp
+++ manifests/global.pp
@@ -3,11 +3,14 @@
 # Usage: go::global { '1.9.0': }
 class go::global($version = '1.1.1') {
   require go
-  $klass = join(['go', join(split($version, '[.]'), '_')], '::')
-  require $klass
+
+  ensure_resource('go::version', $version)
+  $require = Go::Version[$version]
 
   file { "${go::chgo_root}/version":
     content => "${version}\n",
+    mode    => 0644,
     replace => true,
+    require => $require,
   }
 }
